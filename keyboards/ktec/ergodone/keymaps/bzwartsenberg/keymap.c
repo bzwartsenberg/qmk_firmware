3b4b07c50f03da536de96f042269e33840d29e51
Berend Zwartsenberg <berend@zwartsenberg.nl>
2022-01-22 14:45:43 -0800

    Add mod-tap keys on symbol and regular layer

--- keyboards/ktec/ergodone/keymaps/bzwartsenberg/keymap.c
+++ keyboards/ktec/ergodone/keymaps/bzwartsenberg/keymap.c
@@ -42,25 +42,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox( //layer 0 : default
         // left hand
-        KC_GRV,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_MINS,
-        KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC,
-        KC_ESC,  KC_A, KC_S, KC_D, KC_F, KC_G,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_GRV,
-        KC_LCTL, KC_LGUI, KC_LALT, MO(3), MO(1),
-        KC_LEFT, KC_RGHT,
+        KC_GRV ,  KC_1       , KC_2        , KC_3           , KC_4          , KC_5           , KC_MINS,
+        KC_TAB ,  KC_Q       , KC_W        , KC_E           , KC_R          , KC_T           , KC_LBRC,
+        KC_ESC ,  KC_A       , KC_S        , KC_D           , KC_F          , KC_G           ,
+        KC_LSFT, LSFT_T(KC_Z), LCTL_T(KC_X), LGUI_T(KC_C)   , LALT_T(KC_V)  , KC_B           , KC_GRV ,
+        KC_LCTL, KC_LGUI     , KC_LALT     , MO(3)          , MO(1)         ,
+        KC_LEFT, KC_RGHT     ,
         KC_HOME,
-        KC_ENT, KC_TAB, KC_END,
+        KC_ENT , KC_TAB      , KC_END      ,
 
 
         // right hand
-        KC_EQL, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
-        KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
-        KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-        KC_QUOT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-        TT(2), TT(4), KC_LALT, KC_LGUI, KC_LCTL,
-        KC_UP, KC_DOWN,
+        KC_EQL , KC_6        , KC_7        , KC_8           , KC_9          , KC_0           , KC_BSPC,
+        KC_RBRC, KC_Y        , KC_U        , KC_I           , KC_O          , KC_P           , KC_BSLS,
+        KC_H   , KC_J        , KC_K        , KC_L           , KC_SCLN       , KC_QUOT        ,
+        KC_QUOT, KC_N        , LALT_T(KC_M), LGUI_T(KC_COMM), LCTL_T(KC_DOT), LSFT_T(KC_SLSH), KC_RSFT,
+        TT(2)  , TT(4)       , KC_LALT     , KC_LGUI        , KC_LCTL       ,
+        KC_UP  , KC_DOWN     ,
         KC_PGUP,
-        KC_PGDN, KC_BSPC, KC_SPC),
+        KC_PGDN, KC_BSPC     , KC_SPC)     ,
 
 /* Keymap 1: Symbol Layer
  *
@@ -86,24 +86,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
-        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_TRNS,
-        KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_TRNS,
-        KC_TRNS, KC_DLR, KC_PLUS, KC_LPRN, KC_RPRN, KC_AT,
-        KC_TRNS, KC_EXLM, KC_HASH, KC_LCBR, KC_RCBR, KC_TILD, KC_TRNS,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_NO,
-        KC_NO, KC_NO,
-        KC_NO,
-        KC_NO, KC_NO, KC_NO,
+        KC_TRNS, KC_F1          , KC_F2          , KC_F3          , KC_F4          , KC_F5          , KC_TRNS,
+        KC_TRNS, KC_1           , KC_2           , KC_3           , KC_4           , KC_5           , KC_TRNS,
+        KC_TRNS, KC_DLR         , KC_PLUS        , KC_LPRN        , KC_RPRN        , KC_AT          ,
+        KC_TRNS, LSFT_T(KC_EXLM), LCTL_T(KC_HASH), LGUI_T(KC_LCBR), LALT_T(KC_RCBR), KC_TILD        , KC_TRNS,
+        KC_LCTL, KC_LGUI        , KC_LALT        , KC_NO          , KC_NO          ,
+        KC_NO  , KC_NO          ,
+        KC_NO  ,
+        KC_NO  , KC_NO          , KC_NO          ,
 
        // right hand
-        KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-        KC_TRNS, KC_6, KC_7, KC_8, KC_9, KC_0, KC_F12,
-        KC_ASTR, KC_MINS, KC_EQL, KC_UNDS, KC_CIRC, KC_NO,
-        KC_TRNS, KC_AMPR, KC_LBRC, KC_RBRC, KC_PERC, KC_PIPE, KC_NO,
-        KC_NO, KC_NO, KC_LALT, KC_LGUI, KC_LCTL,
-        KC_NO, KC_NO,
-        KC_NO,
-        KC_NO, KC_NO, KC_NO),
+        KC_TRNS, KC_F6          , KC_F7          , KC_F8          , KC_F9          , KC_F10         , KC_F11 ,
+        KC_TRNS, KC_6           , KC_7           , KC_8           , KC_9           , KC_0           , KC_F12 ,
+        KC_ASTR, KC_MINS        , KC_EQL         , KC_UNDS        , KC_CIRC        , KC_NO          ,
+        KC_TRNS, KC_AMPR        , LALT_T(KC_LBRC), LGUI_T(KC_RBRC), LCTL_T(KC_PERC), LSFT_T(KC_PIPE), KC_NO  ,
+        KC_NO  , KC_NO          , KC_LALT        , KC_LGUI        , KC_LCTL        ,
+        KC_NO  , KC_NO          ,
+        KC_NO  ,
+        KC_NO  , KC_NO          , KC_NO)         ,
 /* Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
@@ -127,20 +127,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // MOUSE
 [MOUSE] = LAYOUT_ergodox(
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_WH_U, KC_TRNS, KC_ACL2, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_WH_L, KC_WH_D, KC_WH_R, KC_ACL1, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_ACL0, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_WH_U , KC_TRNS, KC_ACL2, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_WH_L, KC_WH_D , KC_WH_R, KC_ACL1, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_ACL0, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS,
       KC_TRNS, KC_TRNS,
       KC_TRNS,
-      KC_BTN1, KC_BTN3, KC_TRNS,
+      KC_BTN1, KC_BTN3, KC_TRNS ,
 
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_TRNS,
-      TO(0), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_PSCR , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_MS_L, KC_MS_D, KC_MS_U , KC_MS_R, KC_TRNS, KC_TRNS,
+      TO(0)  , KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS,
       KC_TRNS, KC_TRNS,
       KC_TRNS,
       KC_BTN5, KC_BTN4, KC_BTN2),
@@ -169,23 +169,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 //NAV and MEDIA
 [NAV] = LAYOUT_ergodox(
-      KC_NO, KC_BRID, KC_BRIU, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,
-      KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_NO,
-      KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO,
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-      KC_NO, KC_NO,
-      KC_NO,
-      KC_NO, KC_NO, KC_NO,
+      KC_NO  , KC_BRID, KC_BRIU, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,
+      KC_NO  , KC_NO  , KC_UP  , KC_NO  , KC_NO  , KC_NO  , KC_NO,
+      KC_NO  , KC_LEFT, KC_DOWN, KC_RGHT, KC_NO  , KC_NO  ,
+      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO,
+      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+      KC_NO  , KC_NO  ,
+      KC_NO  ,
+      KC_NO  , KC_NO  , KC_NO  ,
 
-      KC_NO, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO,
-      KC_NO, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, KC_NO,
-      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
-      TO(0), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-      KC_NO, KC_NO,
-      KC_NO,
-      KC_NO, KC_NO, KC_NO),
+      KC_NO  , KC_NO  , KC_MUTE, KC_VOLD, KC_VOLU, KC_NO  , KC_NO,
+      KC_NO  , KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_NO  , KC_NO,
+      KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_NO  , KC_NO  ,
+      TO(0)  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO,
+      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+      KC_NO  , KC_NO  ,
+      KC_NO  ,
+      KC_NO  , KC_NO  , KC_NO) ,
 
 
 
