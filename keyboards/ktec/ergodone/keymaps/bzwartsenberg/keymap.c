5a3eba107dfbc767d0d4d8b62d1c04f2cdd96ba5
Berend Zwartsenberg <berend.zwartsenberg@inverted.ai>
2022-03-12 12:58:00 -0800

    Add cvat layer and more print screen options

--- keyboards/ktec/ergodone/keymaps/bzwartsenberg/keymap.c
+++ keyboards/ktec/ergodone/keymaps/bzwartsenberg/keymap.c
@@ -6,6 +6,7 @@
 #define MOUSE 2 // media keys
 #define NAV 4 // nav keys
 #define NUM 3 // number keys
+#define CVAT 5 // cvat
 
 #define TAPPING_TOGGLE 2
 
@@ -13,7 +14,10 @@ enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
   EPRM,
   VRSN,
-  RGB_SLD
+  PSCR_SEL_CP, // printscreen select, and copy to clipboard
+  PSCR_SEL_SV, // printscreen select and save
+  ALT_F4, // close current window
+  RGB_SLD,
 };
 
 const uint16_t PROGMEM combo_quot[] = {KC_L, KC_SCLN, COMBO_END};
@@ -75,25 +79,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox( //layer 0 : default
         // left hand
-        KC_GRV    ,  KC_1   , KC_2    , KC_3      , KC_4     , KC_5      , KC_MINS,
-        KC_TAB    ,  KC_Q   , KC_W    , KC_E      , KC_R     , KC_T      , KC_LBRC,
-        KC_ESC    ,  KC_A   , KC_S    , KC_CTLD   , KC_SFTF  , KC_G      ,
-        KC_LSFT   , KC_SFTZ , KC_CTLX ,  KC_GUIC  , KC_ALTV  , KC_B      , KC_GRV ,
-        KC_LCTL   , KC_LGUI , KC_LALT , MO(3)     , MO(1)    ,
-        KC_LEFT   , KC_RGHT ,
-        KC_HOME   ,
-        KC_ENT    , KC_TAB  , KC_END  ,
+        KC_GRV     , KC_1    , KC_2    , KC_3      , KC_4     , KC_5      , KC_DEL   ,
+        KC_TAB     , KC_Q    , KC_W    , KC_E      , KC_R     , KC_T      , KC_LBRC  ,
+        KC_ESC     , KC_A    , KC_S    , KC_CTLD   , KC_SFTF  , KC_G      ,
+        KC_LSFT    , KC_SFTZ , KC_CTLX , KC_GUIC   , KC_ALTV  , KC_B      , TO(CVAT) ,
+        KC_LCTL    , KC_LGUI , KC_LALT , MO(3)     , MO(1)    ,
+        KC_LEFT    , KC_RGHT ,
+        KC_HOME    ,
+        KC_ENT     , KC_TAB  , KC_END  ,
 
 
         // right hand
-        KC_EQL    , KC_6    , KC_7    , KC_8      , KC_9     , KC_0      , KC_BSPC,
-        KC_RBRC   , KC_Y    , KC_U    , KC_I      , KC_O     , KC_P      , KC_BSLS,
-        KC_H      , KC_SFTJ , KC_CTLK , KC_L      , KC_SCLN  , KC_QUOT   ,
-        KC_QUOT   , KC_N    , KC_ALTM , KC_GUICOMM, KC_CTLDOT, KC_SFTSLSH, KC_RSFT,
-        KC_LT4GRV , TT(2)   , KC_LALT , KC_LGUI   , KC_LCTL  ,
-        KC_UP     , KC_DOWN ,
-        KC_PGUP   ,
-        KC_PGDN   , KC_BSPC , KC_SPC) ,
+        PSCR_SEL_CP, KC_6    , KC_7    , KC_8      , KC_9     , KC_0      , KC_BSPC  ,
+        KC_RBRC    , KC_Y    , KC_U    , KC_I      , KC_O     , KC_P      , KC_BSLS  ,
+        KC_H       , KC_SFTJ , KC_CTLK , KC_L      , KC_SCLN  , KC_QUOT   ,
+        KC_QUOT    , KC_N    , KC_ALTM , KC_GUICOMM, KC_CTLDOT, KC_SFTSLSH, KC_RSFT  ,
+        KC_LT4GRV  , TT(2)   , KC_LALT , KC_LGUI   , KC_LCTL  ,
+        KC_UP      , KC_DOWN ,
+        KC_PGUP    ,
+        KC_PGDN    , KC_BSPC , KC_SPC) ,
 
 /* Keymap 1: Symbol Layer
  *
@@ -119,24 +123,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
-        KC_TRNS, KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_TRNS,
-        KC_TRNS, KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_TRNS,
-        KC_TRNS, KC_DLR  , KC_PLUS , KC_LPRN , KC_RPRN , KC_AT   ,
-        KC_TRNS, KC_EXLM , KC_HASH , KC_LCBR , KC_RCBR , KC_TILD , KC_TRNS,
-        KC_LCTL, KC_LGUI , KC_LALT , KC_NO   , KC_NO   ,
-        KC_NO  , KC_NO   ,
-        KC_NO  ,
-        KC_NO  , KC_NO   , KC_NO   ,
+        KC_NO      , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , ALT_F4 ,
+        KC_NO      , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_NO  ,
+        KC_NO      , KC_DLR  , KC_PLUS , KC_LPRN , KC_RPRN , KC_AT   ,
+        KC_NO      , KC_EXLM , KC_HASH , KC_LCBR , KC_RCBR , KC_TILD , KC_NO  ,
+        KC_LCTL    , KC_LGUI , KC_LALT , KC_NO   , KC_NO   ,
+        KC_NO      , KC_NO   ,
+        KC_NO      ,
+        KC_NO      , KC_NO   , KC_NO   ,
 
        // right hand
-        KC_TRNS, KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10 , KC_F11 ,
-        KC_TRNS, KC_6    , KC_7    , KC_8    , KC_9    , KC_0   , KC_F12 ,
-        KC_ASTR, KC_MINS , KC_EQL  , KC_UNDS , KC_CIRC , KC_NO  ,
-        KC_TRNS, KC_AMPR , KC_LBRC , KC_RBRC , KC_PERC , KC_PIPE, KC_NO  ,
-        KC_NO  , KC_NO   , KC_LALT , KC_LGUI , KC_LCTL ,
-        KC_NO  , KC_NO   ,
-        KC_NO  ,
-        KC_NO  , KC_NO   , KC_NO)  ,
+        PSCR_SEL_SV, KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11 ,
+        KC_NO      , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_F12 ,
+        KC_ASTR    , KC_MINS , KC_EQL  , KC_UNDS , KC_CIRC , KC_NO   ,
+        KC_NO      , KC_AMPR , KC_LBRC , KC_RBRC , KC_PERC , KC_PIPE , KC_NO  ,
+        KC_NO      , KC_NO   , KC_LALT , KC_LGUI , KC_LCTL ,
+        KC_NO      , KC_NO   ,
+        KC_NO      ,
+        KC_NO      , KC_NO   , KC_NO)  ,
 /* Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
@@ -160,28 +164,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // MOUSE
 [MOUSE] = LAYOUT_ergodox(
-      KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_WH_U , KC_TRNS, KC_ACL2, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_WH_L, KC_WH_D , KC_WH_R, KC_ACL1, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_ACL0, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS,
-      KC_TRNS,
-      KC_BTN1, KC_BTN3, KC_TRNS ,
+      KC_NO  , KC_NO  , KC_NO   , KC_NO  , KC_NO  , KC_NO, KC_NO,
+      KC_NO  , KC_NO  , KC_WH_U , KC_NO  , KC_ACL2, KC_NO, KC_NO,
+      KC_NO  , KC_WH_L, KC_WH_D , KC_WH_R, KC_ACL1, KC_NO,
+      KC_NO  , KC_NO  , KC_NO   , KC_NO  , KC_ACL0, KC_NO, KC_NO,
+      KC_NO  , KC_NO  , KC_NO   , KC_NO  , KC_NO  ,
+      KC_NO  , KC_NO  ,
+      KC_NO  ,
+      KC_BTN1, KC_BTN3, KC_NO   ,
 
-      KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_DEL , KC_PSCR , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_MS_L, KC_MS_D, KC_MS_U , KC_MS_R, KC_TRNS, KC_TRNS,
-      TO(0)  , KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS,
-      KC_TRNS,
+      KC_NO  , KC_NO  , KC_NO   , KC_NO  , KC_NO  , KC_NO, KC_NO,
+      KC_NO  , KC_NO  , KC_PSCR , KC_NO  , KC_NO  , KC_NO, KC_NO,
+      KC_MS_L, KC_MS_D, KC_MS_U , KC_MS_R, KC_NO  , KC_NO,
+      TO(0)  , KC_NO  , KC_NO   , KC_NO  , KC_NO  , KC_NO, KC_NO,
+      KC_NO  , KC_NO  , KC_NO   , KC_NO  , KC_NO  ,
+      KC_NO  , KC_NO  ,
+      KC_NO  ,
       KC_BTN5, KC_BTN4, KC_BTN2),
 
 /*
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        | BrDn | BrUp | MPrev| Mplay|Mnext |      |           |      |      |      |      |      |      |        |
+ * |        | BrDn | BrUp | MPrev| Mplay|Mnext |      |           |      |      | Mute |VolD  |VolU  |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |  Up  |      |      |      |      |           |      |Home  |pgdn  |Pgup  |End   |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -248,7 +252,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [NUM] = LAYOUT_ergodox(
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-      KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5,
+      KC_NO, KC_1 , KC_2 , KC_3 , KC_4 , KC_5 ,
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
       KC_NO, KC_NO,
@@ -257,12 +261,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-             KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
+      KC_6 , KC_7 , KC_8 , KC_9 , KC_0 , KC_NO,
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
       KC_NO, KC_NO,
       KC_NO,
-      KC_NO, KC_NO, KC_NO)
+      KC_NO, KC_NO, KC_NO),
+
+[CVAT] = LAYOUT_ergodox(
+      KC_NO, KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO   ,
+      KC_NO, KC_NO , KC_M  , KC_N  , KC_NO , KC_NO , KC_NO   ,
+      KC_NO, KC_NO , KC_D  , KC_F  , KC_NO , KC_NO ,
+      KC_NO, KC_DEL, KC_C  , KC_V  , KC_NO , KC_NO , TO(BASE),
+      KC_NO, KC_NO , KC_NO , KC_NO , KC_NO ,
+      KC_NO, KC_NO ,
+      KC_NO,
+      KC_NO, KC_NO , KC_NO ,
+
+      KC_NO, KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO   ,
+      KC_NO, KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO   ,
+      KC_NO, KC_NO , KC_NO , KC_NO , KC_NO , KC_NO ,
+      KC_NO, KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO   ,
+      KC_NO, KC_NO , KC_NO , KC_NO , KC_NO ,
+      KC_NO, KC_NO ,
+      KC_NO,
+      KC_NO, KC_NO , KC_NO)
+
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -285,6 +309,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         #ifdef RGBLIGHT_ENABLE
           rgblight_mode(1);
         #endif
+      }
+      return false;
+      break;
+
+    case PSCR_SEL_CP:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTRL(SS_LSFT(SS_TAP(X_PSCR))));
+      }
+      return false;
+      break;
+
+    case PSCR_SEL_SV:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LSFT(SS_TAP(X_PSCR)));
+      }
+      return false;
+      break;
+
+    case ALT_F4:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LALT(SS_TAP(X_F4)));
       }
       return false;
       break;
